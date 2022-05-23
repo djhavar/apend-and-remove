8e6a17dfd4aa2acf35248c9cf5095d57564c28b7
djhavar <90192071+djhavar@users.noreply.github.com>
2022-05-23 16:54:41 +0530

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,20 @@
+numbers=['1','2','3']
+print(numbers)
+
+numbers.append('4')
+print('updated numbers list',numbers)
+
+more_numbers=['5','6','7']
+print(more_numbers)
+
+numbers.append(more_numbers)
+print('updated numbers list',numbers)
+
+numbers=['1','2','3']
+print(numbers)
+
+numbers.remove('1')
+print(numbers)
+
+
+
